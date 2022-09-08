a82314cfe4f6fbfa69b32040e4acd7598c3388f0
duniyal <duniyalr@gmail.com>
2022-09-08 23:21:39 +0430

    fix typeorm filter

--- src/modules/log/log.service.ts
+++ src/modules/log/log.service.ts
@@ -30,32 +30,33 @@ export class LogService {
   async index(indexLogDto: IndexLogDto) {
     const queryBuilder = this.dataSource.getRepository(Log)
       .createQueryBuilder("log")
-      .innerJoinAndSelect("log.section","section")
-      .innerJoinAndSelect("section.project", "project")
+      .innerJoinAndSelect("log.section","s")
+      .innerJoinAndSelect("s.project", "project")
     
 
-    if (indexLogDto.projectId) {
-      queryBuilder.andWhere("project.id = :id", {id: indexLogDto.projectId});
-    }
+      if (indexLogDto.projectId) {
+        queryBuilder.andWhere("project.id = :projectId", {projectId: indexLogDto.projectId});
+      }
 
     if(indexLogDto.sectionId) {
-      queryBuilder.andWhere("section.id = :id", {id: indexLogDto.sectionId});
+      console.log("yes sectionId", indexLogDto.sectionId)
+      queryBuilder.andWhere("s.id = :sectionId", {sectionId: indexLogDto.sectionId});
     }
 
     if (indexLogDto.q) {
-      queryBuilder.where("log.url like :like", {like: `%${scapeSqlLikeOperator(indexLogDto.q)}%`});
+      queryBuilder.andWhere("log.url like :like", {like: `%${scapeSqlLikeOperator(indexLogDto.q)}%`});
     }
 
     let sortBy;
     switch(indexLogDto.sortBy) {
       case "createdAt":
-        sortBy = "createdAt"
+        sortBy = "log.createdAt"
         break;
       case "projectId":
         sortBy = "project.id";
         break;
       case "sectionId":
-        sortBy = "section.id";
+        sortBy = "s.id";
     }
 
     queryBuilder.orderBy(sortBy, indexLogDto.sortOrder);
@@ -63,7 +64,7 @@ export class LogService {
     queryBuilder
     .take(indexLogDto.take)
     .skip(indexLogDto.skip)
-    
+
     const itemCount = await queryBuilder.getCount();
     const { entities } = await queryBuilder.getRawAndEntities();
 
